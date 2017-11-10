820eaa7ac042a24f0ec8031b8ce7afcf4ec27c34
Stefanie Brabec <sbrabec@me.com>
2017-11-10 20:34:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-function sayHiToGrandma(string){
-  if ('hello') return('I can\'t hear you!')
-  if ('HELLO') return('YES INDEED!')
-  if('I love you, Grandma.') return('I love you, too.')
+function shout(string) {
+  return string.toUpperCase
 }
