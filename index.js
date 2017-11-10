3ccff278f6601091e3b2fb3ccfabdc8d75bde11f
Stefanie Brabec <sbrabec@me.com>
2017-11-10 21:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if ("hello") return("I can\'t hear you!")
 }
+function sayHiToGrandma(string) {
+  if ("HELLO") return("YES INDEED!")
+}
